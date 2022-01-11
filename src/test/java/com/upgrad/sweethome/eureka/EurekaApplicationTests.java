ecfb087070ed6543b4f9efeaefd279525fb3635e
Preneeth <preneetho@gmail.com>
2022-01-11 19:01:57 +0530

    Initial

---
+++ src/test/java/com/upgrad/sweethome/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrad.sweethome.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
